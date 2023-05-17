2cc3c143808b193b40c2c414baf0b3d87090dc62
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-17 16:02:15 +0200

    add Jumbotron bonus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
   import AppTitle from "./components/AppTitle.vue"
+  import Jumbotron from "./components/Jumbotron.vue"
+
 
   export default {
     components: {
       AppTitle,
+      Jumbotron,
     }
   }
 </script>
 
 <template>
   <AppTitle/>
+  <Jumbotron/>
 </template>
 
 <style scoped>
